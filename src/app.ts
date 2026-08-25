808d0e5ef6a23a0d75bee9f32356846c10029d5d
adil2024 <163632468+adil2024@users.noreply.github.com>
2026-08-25 20:33:33 +0530

    correct path

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import  statusCodes from "../src/utils/statusCodes"
-import routes from "./routes";
-import { errorMiddleware } from "./middlewares/error.middleware";
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import statusCodes from './utils/statusCodes';
+import routes from './routes';
+import { errorMiddleware } from './middlewares/error.middleware';
 
 const app = express();
 
@@ -12,12 +12,12 @@ app.use(cors());
 app.use(express.json());
 
 //route
-app.use("/api/v1", routes);
+app.use('/api/v1', routes);
 
-app.get("/health", (_req, res) => {
+app.get('/health', (_req, res) => {
   res.status(statusCodes.OK).json({
     success: true,
-    message: "TaskFlow API is running",
+    message: 'TaskFlow API is running',
   });
 });
 
